11b8d55916b318ec2d5199cb9b7c315b9b4b1074
03Zelora03 <2127160@bdeb.qc.ca>
2023-10-09 21:32:18 -0400

    Création page d'accueil

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'clean': ['Source Sans Pro', 'sans-serif']
+    },
+    colors:{
+      'text': '#e6e6f4',
+      'background': '#06060e',
+      'light-background': '#0E0E16',
+      'primary': '#0f5756',
+      'secondary': '#0b0c41',
+      'accent': '#3bdedb',
+    },
     extend: {},
   },
   plugins: [],
