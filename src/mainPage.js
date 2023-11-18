2ff2a766430fa964e22aa4d5ac57b1e68719af28
03Zelora03 <2127160@bdeb.qc.ca>
2023-11-17 22:25:18 -0500

    Ajout nouvelle page

---
+++ src/mainPage.js
@@ -0,0 +1,17 @@
+function cliquePiece(piece){
+    console.log("clique")
+    switch(piece){
+        case "StackOverBros":
+            console.log("SOB")
+            window.location.href = "./pieces/sob/pieceSOB.html"
+            break
+        case "Lunar Strike":
+            console.log("LS")
+            break
+        case "Swift Shift":
+            console.log("SS")
+            break
+        default:
+            break
+    }
+}
